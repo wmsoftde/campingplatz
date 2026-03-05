db4c6646818a5255fecb0b755ea5c02b991bf7b4
wmsoftde <43696848+wmsoftde@users.noreply.github.com>
2026-03-05 18:54:44 +0100

    chore: bind server on 0.0.0.0 in production

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
+const hostname = dev ? 'localhost' : '0.0.0.0';
 const port = parseInt(process.env.PORT || '3000', 10);
 
 const app = next({ dev, hostname, port });
